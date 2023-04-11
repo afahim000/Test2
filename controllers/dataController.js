6675b36727f6e48649fd3fdacf55b9f17f11fbea
CesarIgnacio <cesarignac@gmail.com>
2023-04-11 17:49:36 -0400

    test cip

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -2,23 +2,20 @@ const express = require('express');
 const animalSchema = require('../models/modified model');
 const path = require('path');
 const multer = require('multer');
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 const { check, validationResult } = require("express-validator");
 const textOnly = multer();
 
 const images = multer.diskStorage({
 
-	destination: function (req, file, callback){
-		if(req.body.filetypes === 'images')
-		{
+	destination: function (req, file, callback) {
+		if (req.body.filetypes === 'images') {
 			callback(null, './dev-data/img');
 		}
-		else if(req.body.filetypes === 'video')
-		{
+		else if (req.body.filetypes === 'video') {
 			callback(null, './dev-data/videos');
 		}
-		else
-		{
+		else {
 			callback(null, './dev-data/audio');
 		}
 	},
@@ -57,7 +54,7 @@ const audioUpload = multer({
 //const vidUpload;
 //const audioUpload;
 exports.getOptions = (req, res) => {
-	res.status(200).render('buttons', { title: 'buttons'});
+	res.status(200).render('buttons', { title: 'buttons' });
 };
 
 exports.queryOptionsGet = (req, res) => {
@@ -69,9 +66,8 @@ exports.deleteGet = (req, res) => {
 };
 
 
-exports.addDataGet = (req, res) => 
-{
-	
+exports.addDataGet = (req, res) => {
+
 	res.status(200).render('tester', { title: 'tester' });
 };
 
@@ -81,38 +77,57 @@ exports.queryOptionsPost = (req, res) => {
 
 exports.addDataPost = (req, res) => {
 
-		imgUpload(req, res, (err) => {
+	imgUpload(req, res, (err) => {
 		if (err) {
 			res.render('tester');
 			res.send('MEOWWWW!!!!');
 		}
 		else {
 			let fileLocation = req.file.destination;
-			const count = Math.random()*10000;
-			console.log(count);
+
+			// Collecting address information and storing it in the arrAdd
+			// const addressF = /^(\d* [a-zA-Z .]*), ([a-zA-Z ]*), ([A-Z]{2}) (\d{5})$/;
+			// const arrAdd = req.body.address.match(addressF);
+
+			// Creating date object
+			// const startDate = new Date(`${req.body.sdate}T${req.body.stime}`);
+			// const endDate = new Date(`${req.body.sdate}T${req.body.etime}`);
+
+			// Creating a new data entry following the schema in the file 'models/modified model.js'
 			const data = new animalSchema({
-			id: count + 1,
-			animalName: req.body.animallist,
-			recorder: 
-			{
-				last: req.body.lname,
-				first: req.body.fname
-			},
-			location: 
-			{
-				latitude:req.body.latitude,
-				longitude: req.body.longitude
-			},
-			mediaType: req.body.filetypes,
-			fileDirectory: `.${fileLocation}/${req.file.filename}`
+
+				animalName: req.body.animallist,
+				recorder:
+				{
+					last: req.body.lname,
+					first: req.body.fname
+				},
+				// date:
+				// {
+				// 	start: startDate,
+				// 	end: endDate
+				// },
+				location:
+				{
+					//country: 'USA',
+					//state: arrAdd[3],
+					//city: arrAdd[2],
+					//street: arrAdd[1],
+					//zipcode: arrAdd[4],
+					latitude: req.body.latitude,
+					longitude: req.body.longitude
+
+				},
+				mediaType: req.body.filetypes,
+				fileDirectory: `.${fileLocation}/${req.file.filename}`
 			});
 			data.save();
 			//res.send('test');
 
-			}
-									});
+		}
+	});
 
-		
+
 };
 
 exports.deletePost = (req, res) => {
